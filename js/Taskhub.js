4bba7a05a118f49e83de89897dd17657c956b380
Arthrrr <ajj1726@hotmail.com>
2023-03-21 21:27:23 -0600

    agregue el login al github

---
+++ js/Taskhub.js
@@ -0,0 +1,29 @@
+//Sirve para seleccionar elementos específicos del documento HTML y asignarlos a variables en JavaScript, lo que permite manipularlos o interactuar con ellos a través de código
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon-password"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bxs-hide", "bxs-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bxs-show", "bxs-hide");
+        })
+
+
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e =>{
+        e.preventDefault(); //preventing form submit
+        forms.classList.toggle("show-signup");
+    })
+})
