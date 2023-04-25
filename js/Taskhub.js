688aff05423bcdf6cc1b18ccbfe367877bb3f47a
Garbba <106709961+Garbba@users.noreply.github.com>
2023-04-25 00:01:47 -0600

    Login and Sign up Finished

--- js/Taskhub.js
+++ js/Taskhub.js
@@ -1,30 +1,24 @@
-//Sirve para seleccionar elementos específicos del documento HTML y asignarlos a variables en JavaScript, lo que permite manipularlos o interactuar con ellos a través de código
-const forms = document.querySelector(".forms"),
-      pwShowHide = document.querySelectorAll(".eye-icon-password"),
-      links = document.querySelectorAll(".link");
+const forms = document.querySelector(".forms");
+const pwShowHide = document.querySelectorAll(".eye-icon-password");
+links = document.querySelectorAll(".link");
 
-      pwShowHide.forEach(eyeIcon => {
-        eyeIcon.addEventListener("click", () => {
-          let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-      
-          pwFields.forEach(password => {
-            if (password.type === "password") {
-              password.type = "text";
-              eyeIcon.classList.replace("bxs-hide", "bxs-show");
-            } else {
-              password.type = "password";
-              eyeIcon.classList.replace("bxs-show", "bxs-hide");
-            }
-          });
-        });
-      });
+    pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwField = eyeIcon.previousElementSibling;
 
-
-
-
+        if (pwField.type === "password") {
+        pwField.type = "text";
+        eyeIcon.classList.replace("bxs-hide", "bxs-show");
+        } else {
+        pwField.type = "password";
+        eyeIcon.classList.replace("bxs-show", "bxs-hide");
+        }
+    });
+    });
+ 
 links.forEach(link => {
     link.addEventListener("click", e =>{
-        e.preventDefault(); //preventing form submit
+        e.preventDefault(); 
         forms.classList.toggle("show-signup");
     })
 })
--- js/loginValidation.js
+++ js/loginValidation.js
@@ -1,4 +1,3 @@
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyB2G2mqCwNPa0nskmzxGcNvGBNgOT5l540",
     authDomain: "to-do-list-dce75.firebaseapp.com",
@@ -7,38 +6,28 @@ const firebaseConfig = {
     messagingSenderId: "162155799901",
     appId: "1:162155799901:web:2db127c184835d16e1f4b4"
   };
-  // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  // Initialize variables
   const auth = firebase.auth()
   const database = firebase.database()
   
-  // Set up our register function
   function register () {
-    // Get all our input fields
     email = document.getElementById('email').value
     password = document.getElementById('password').value
     full_name = document.getElementById('full_name').value
    
   
-    // Validate input fields
     if (validate_email(email) == false || validate_password(password) == false) {
       alert('Por favor ingrese todos los campos')
       return
-      // Don't continue running the code
     }
   
    
-    // Move on with Auth
     auth.createUserWithEmailAndPassword(email, password)
     .then(function() {
-      // Declare user variable
       var user = auth.currentUser
   
-      // Add this user to Firebase Database
       var database_ref = database.ref()
   
-      // Create User data
       var user_data = {
         email : email,
         full_name : full_name,
@@ -60,42 +49,32 @@ const firebaseConfig = {
     })
   }
   
-  // Set up our login function
   function login () {
-    // Get all our input fields
     email = document.getElementById('email2').value
     password = document.getElementById('password2').value
   
-    // Validate input fields
     if (validate_email(email) == false || validate_password(password) == false) {
       alert('Por favor ingrese todos los campos')
       return
-      // Don't continue running the code
     }
   
     auth.signInWithEmailAndPassword(email, password)
     .then(function() {
-      // Declare user variable
       var user = auth.currentUser
   
-      // Add this user to Firebase Database
       var database_ref = database.ref()
   
-      // Create User data
       var user_data = {
         last_login : Date.now()
       }
   
-      // Push to Firebase Database
       database_ref.child('users/' + user.uid).update(user_data)
   
-      // DOne
       alert('Bienvenido')
       window.location.href = "todo.html";
   
     })
     .catch(function(error) {
-      // Firebase will use this to alert of its errors
       var error_code = error.code
       var error_message = error.message
       
@@ -105,14 +84,11 @@ const firebaseConfig = {
   
   
   
-  // Validate Functions
   function validate_email(email) {
     expression = /^[^@]+@\w+(\.\w+)+\w$/
     if (expression.test(email) == true) {
-      // Email is good
       return true
     } else {
-      // Email is not good
       return false
     }
   }
