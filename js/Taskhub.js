a7f85ed49235a1690263dfe824c54d40bfc1a022
Garbba <106709961+Garbba@users.noreply.github.com>
2023-04-24 21:52:38 -0600

    login validation

--- js/Taskhub.js
+++ js/Taskhub.js
@@ -3,23 +3,41 @@ const forms = document.querySelector(".forms"),
       pwShowHide = document.querySelectorAll(".eye-icon-password"),
       links = document.querySelectorAll(".link");
 
-pwShowHide.forEach(eyeIcon => {
-    eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
-
-        pwFields.forEach(password => {
-            if(password.type === "password"){
-                password.type = "text";
-                eyeIcon.classList.replace("bxs-hide", "bxs-show");
-                return;
+      pwShowHide.forEach(eyeIcon => {
+        eyeIcon.addEventListener("click", () => {
+          let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+      
+          pwFields.forEach(password => {
+            if (password.type === "password") {
+              password.type = "text";
+              eyeIcon.classList.replace("bxs-hide", "bxs-show");
+            } else {
+              password.type = "password";
+              eyeIcon.classList.replace("bxs-show", "bxs-hide");
             }
-            password.type = "password";
-            eyeIcon.classList.replace("bxs-show", "bxs-hide");
-        })
+          });
+        });
+      });
+    
+
+    //   pwShowHide.forEach(eyeIcon => {
+    //     eyeIcon.addEventListener("click", () => {
+    //         let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll('input[type="password"]');
+    
+    //         pwFields.forEach(password => {
+    //             if(password.type === "password"){
+    //                 password.type = "text";
+    //                 eyeIcon.classList.replace("bxs-hide", "bxs-show");
+    //                 return;
+    //             }
+    //             password.type = "password";
+    //             eyeIcon.classList.replace("bxs-show", "bxs-hide");
+    //         })
+    
+    //     })
+    // })
 
 
-    })
-})
 
 links.forEach(link => {
     link.addEventListener("click", e =>{
